814caaa65530c5b3482a935df6d8c1e3298c9de7
mhhoban <mhhoban@gmail.com>
2018-03-19 23:38:39 -0700

    first test for game service completed

--- dukedoms_integration_tests/features/game_service.feature
+++ dukedoms_integration_tests/features/game_service.feature
@@ -4,9 +4,9 @@ Feature: Game Service
   Scenario: Create New Game
     When service receives request to create new game with properties:
     | host_player | invited_players |
-    | blarg       | targ, plarg     |
+    | blarg       | targ,plarg      |
     Then the game service successfully creates a new game
     When service receives request for that game info
     Then the game service returns a game with info:
     | host_player| invited_players | game_status| pending_players |
-    | blarg      | targ, plarg     | pending    | targ, plarg     |
+    | blarg      | targ,plarg     | pending    | targ,plarg       |
--- dukedoms_integration_tests/features/steps/game_service.py
+++ dukedoms_integration_tests/features/steps/game_service.py
@@ -1,4 +1,5 @@
 from behave import given, then, when
+from hamcrest import assert_that, equal_to, contains_inanyorder, is_in
 
 #def create_new_game_request(client, host_player, invited_players):
 def create_new_game_request(client=None, invited_players=None, host_player=None):
@@ -19,20 +20,23 @@ def step_create_game(context):
     """
     Create a new game with game service
     """
+    for row in context.table:
+        new_game_request = create_new_game_request(
+            client=context.clients.game_service,
+            host_player=row['host_player'],
+            invited_players=row['invited_players'].split(',')
+        )
+        results, status = context.clients.game_service.newGame.create_new_game(
+            newGameRequest=new_game_request
+        ).result()
 
-
-    new_game_request = create_new_game_request(
-        client=context.clients.game_service,
-        host_player='blarg',
-        invited_players=['blarg, targ']
-    )
-    results, status = context.clients.game_service.newGame.create_new_game(
-        newGameRequest=new_game_request
-    ).result()
+        assert_that(status.status_code, equal_to(200))
+        context.status_code = status.status_code
+        context.game_id = results.game_id
 
 @then('the game service successfully creates a new game')
 def assert_game_created(context):
-    pass
+    assert_that(context.status_code, equal_to(200))
 
 @when('service receives request for that game info')
 def step_request_game_info(context):
@@ -40,4 +44,11 @@ def step_request_game_info(context):
 
 @then('the game service returns a game with info')
 def assert_game_info(context):
-    pass
+    results, status = context.clients.game_service.getGame.get_game_info(
+        gameId=context.game_id
+    ).result()
+
+    assert_that(context.table.rows[0]['host_player'], equal_to('blarg'))
+
+    for player in context.table.rows[0]['invited_players'].split(','):
+        assert_that(player, is_in(results.players['invited_players']['invitedPlayers']))
